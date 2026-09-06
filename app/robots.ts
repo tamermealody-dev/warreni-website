077ff64c2d5121c26608686a4234efb694ef90e8
Engmtamer <mt1817221@gmail.com>
2026-09-07 01:42:32 +0300

    added all option

---
+++ app/robots.ts
@@ -0,0 +1,16 @@
+import type { MetadataRoute } from 'next'
+
+const siteUrl = process.env.APP_URL || 'https://warreeni.com'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin', '/api', '/profile', '/messages', '/transactions', '/payout', '/payment', '/sessions', '/reset-password'],
+      },
+    ],
+    sitemap: `${siteUrl}/sitemap.xml`,
+  }
+}
